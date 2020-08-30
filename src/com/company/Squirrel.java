e08fd76304275a129ae38ef5309b078d960124cf
MalyshevDenis <malyshevdenn@gmail.com>
2020-08-30 23:10:10 +0300

    initial commit of the project

---
+++ src/com/company/Squirrel.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+public class Squirrel {
+    public static int squirrel(int N) {
+        int factorial = N;
+        for (int i = 1; i < N; i++) {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+    public static  int firstDigit(int factorial){
+       int result = 1;
+        for (int i = 10; i < factorial ; i = i * 10) {
+            result = factorial/i;
+        }
+        return result;
+    }
+
+
+}
