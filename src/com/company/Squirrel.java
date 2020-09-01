2c281082eb703119dcbea7695a3e96c15cca9cd8
MalyshevDenis <malyshevdenn@gmail.com>
2020-09-01 10:39:31 +0300

    initial commit

--- src/com/company/Squirrel.java
+++ src/com/company/Squirrel.java
@@ -1,7 +1,20 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class Squirrel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int factorial = Squirrel.squirrel(N);
+
+        int result = Squirrel.firstDigit(factorial);
+        System.out.println(result);
+    }
+
+
     public static int squirrel(int N) {
         int factorial = N;
         for (int i = 1; i < N; i++) {
@@ -9,10 +22,11 @@ public class Squirrel {
         }
         return factorial;
     }
-    public static  int firstDigit(int factorial){
-       int result = 1;
-        for (int i = 10; i < factorial ; i = i * 10) {
-            result = factorial/i;
+
+    public static int firstDigit(int factorial) {
+        int result = 1;
+        for (int i = 10; i < factorial; i = i * 10) {
+            result = factorial / i;
         }
         return result;
     }
