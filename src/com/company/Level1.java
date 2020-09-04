4c93e74cd1d2690d645d1d5ec76620c2a6b8cec2
MalyshevDenis <malyshevdenn@gmail.com>
2020-09-04 17:34:22 +0300

    changed all

--- src/com/company/Level1.java
+++ src/com/company/Level1.java
@@ -4,15 +4,13 @@ package com.company;
 public class Level1 {
 
 
-    public static int factorial(int N) {
+    public static int squirrel(int N) {
+
         int value = 1;
         for (int i = 1; i <= N; i++) {
             value = value * i;
         }
-        return value;
-    }
 
-    public static int squirrel(int value) {
         int result = 1;
         for (int i = 1; i <= value; i = i * 10) {
             result = value / i;
