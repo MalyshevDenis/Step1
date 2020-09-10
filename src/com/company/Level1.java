c4d530ec1bbcfac0e1d4f7f209724b3ca5019c8e
MalyshevDenis <malyshevdenn@gmail.com>
2020-09-11 00:29:39 +0300

    changed all

--- src/com/company/Level1.java
+++ src/com/company/Level1.java
@@ -5,30 +5,18 @@ public class Level1 {
 
     static int odometer(int[] oksana) {
 
-        int summaChetnih = 0;
-        int summaNeChetnih = 0;
-        for (int i = 0; i < oksana.length; i++) {
-            if (oksana[i] % 2 == 0) {
-                summaChetnih = summaChetnih + oksana[i];
 
-            }
+        int x1 = oksana[0];
+        int y1 = oksana[1];
+        int x2 = oksana[2];
+        int y2 = oksana[3];
+        int x3 = oksana[4];
+        int y3 = oksana[5];
 
-        }
+        int result = x1 * y1 + x2 * (y2 - y1) + x3 * (y3 - y2);
 
-
-        for (int i = 0; i < oksana.length; i++) {
-            if (oksana[i] % 2 != 0) {
-                summaNeChetnih = summaNeChetnih + oksana[i];
-            }
-
-        }
-
-        int result = summaChetnih * summaNeChetnih;
-        return (result);
-
-
+        return result;
     }
-
 
 
 }
